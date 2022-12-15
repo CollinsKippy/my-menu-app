e9299da23fdfd0f27fc56c8835a95a559a04d6fa
CK <collins@analence.com>
2022-12-15 13:07:08 +0300

    injection tokens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, InjectionToken, Injector } from '@angular/core';
 
+export const BASE_URL = new InjectionToken<string>('Base Url');
+
+const injector = Injector.create({
+  providers: [
+    {
+      provide: BASE_URL,
+      useValue: 'http://localhost: 4200',
+    },
+  ],
+});
+
+const url = injector.get<string>(BASE_URL);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{ provide: BASE_URL, useValue: 'http://localhost: 4300' }],
 })
 export class AppComponent {
   title = 'my-menu-app';
+
+  constructor() {
+    // Using inject from angular/core
+    const url = inject<string>(BASE_URL);
+    console.log({ url });
+
+    const myInjector = Injector.create({
+      providers: [{ provide: BASE_URL, useValue: 'http://localhost: 5400' }],
+    });
+    const newUrl = myInjector.get<string>(BASE_URL);
+    console.log({ newUrl });
+  }
 }
