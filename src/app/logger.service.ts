93657050ef1acef806357113cd73d02a61d87da2
CK <collins@analence.com>
2022-12-16 16:38:54 +0300

    removed @Injectable for the LoggerService

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { ILogger } from './i-logger';
 
-@Injectable({
-  providedIn: 'root',
-})
+// @Injectable({
+//   providedIn: 'root',
+// })
 export class LoggerService implements ILogger {
   constructor() {}
 
   logInfo(val: string): void {
-    console.log(`Logger Service: ${val}`);
+    console.log(`My Logger Service: ${val}`);
   }
 
   logError(val: string): void {
-    console.info(`Logger Service: ${val}`);
+    console.info(`My Logger Service: ${val}`);
   }
 
   logDebug(val: string): void {
-    console.debug(`Logger Service: ${val}`);
+    console.debug(`My Logger Service: ${val}`);
   }
 }
